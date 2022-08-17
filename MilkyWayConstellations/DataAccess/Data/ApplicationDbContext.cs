83966ead9de504e8a5e18b8d37078cde3495f919
Marius Teodorescu <marius.teodorescu@wirtek.com>
2022-08-17 13:10:16 +0300

    Initial commit

---
+++ MilkyWayConstellations/DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Constellation> Constellations { get; set; }
+    }
+}
